8bf27f83ba8d1435cb7d67be858f01c42bfe793b
shashank <shashankmende88@gmail.com>
2023-08-30 08:52:39 +0000

    Delete & like features has to be added

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,48 @@
 // Write your code here
+import {formatDistanceToNow} from 'date-fns'
+import './index.css'
+
+const CommentItem = props => {
+  const {user, bgColorsList} = props
+  const index = parseInt(Math.random() * 6)
+  const colorName = bgColorsList[index]
+  console.log('colorName = ', colorName)
+  const {name, comment} = user
+
+  const initial = name.slice(0, 1)
+
+  return (
+    <li className="list-item">
+      <div className="top-container">
+        <div className={`porfile-icon-container ${colorName}`}>
+          <p className="profile-icon ">{initial}</p>
+        </div>
+        <div className="name-time-comment-container">
+          <div className="name-time-container">
+            <h1 className="heading">{name}</h1>
+            <p className="time">{formatDistanceToNow(new Date())}</p>
+          </div>
+          <p className="comment">{comment}</p>
+        </div>
+      </div>
+      <div className="like-delete-container">
+        <div className="like-delete-inside-container">
+          <button className="like-button" type="button" data-testid="delete">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+              alt="like"
+            />
+          </button>
+          <p>Like</p>
+        </div>
+        <button type="button" className="like-button">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
-import {formatDistanceToNow} from 'date-fns'
+
+import {v4 as uuidv4} from 'uuid'
+import CommentItem from '../CommentItem'
 import './index.css'
 
 const initialContainerBackgroundClassNames = [
@@ -14,47 +16,96 @@ const initialContainerBackgroundClassNames = [
 
 // Write your code here
 class Comments extends Component {
+  state = {commentsList: [], name: '', comment: '', isLiked: false}
+
+  onClickAddBtn = () => {
+    const {name, comment} = this.state
+    const newComment = {
+      name,
+      comment,
+      id: uuidv4(),
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      name: '',
+      comment: '',
+    }))
+    const {commentsList} = this.state
+  }
+
+  onClickNameInput = event => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
+  onChangeTextarea = event => {
+    this.setState({
+      comment: event.target.value,
+    })
+  }
+
   render() {
+    const {commentsList, name, comment} = this.state
+    console.log('commentsList = ', commentsList)
+    const noOfComments = commentsList.length
     return (
       <div className="bg_container">
-        <div className="top-card-horizontal-line">
-          <div className="top-card-responsive-container">
-            <div className="top-card">
-              <h1 className="heading">Comments</h1>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png "
-                alt="comments"
-                className="comments-image small-screen-image"
-              />
+        <div className="top-card">
+          <div>
+            <h1 className="heading">Comments</h1>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png "
+              alt="comments"
+              className="comments-image small-screen-image"
+            />
 
-              <div className="input-textarea-button-container">
-                <p>Say something about 4.0 Technologies</p>
-                <input
-                  type="text"
-                  placeholder="Your Name"
-                  className="inputelement"
-                />
-                <textarea
-                  className="textarea"
-                  placeholder="Your Comment"
-                  cols="40"
-                  rows="10"
-                />
-                <button className="button" type="button">
-                  Add Comment
-                </button>
-              </div>
-            </div>
-
-            <div>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png "
-                alt="comments"
-                className="comments-image large-screen-image"
-              />
-            </div>
+            <p>Say something about 4.0 Technologies</p>
+            <input
+              type="text"
+              value={name}
+              placeholder="Your Name"
+              className="inputelement"
+              onChange={this.onClickNameInput}
+            />
+            <textarea
+              className="textarea"
+              value={comment}
+              placeholder="Your Comment"
+              cols="30"
+              rows="10"
+              onChange={this.onChangeTextarea}
+            />
+            <button
+              className="button"
+              type="button"
+              onClick={this.onClickAddBtn}
+            >
+              Add Comment
+            </button>
           </div>
-          <hr className="horizontal-line" />
+
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png "
+            alt="comments"
+            className="comments-image large-screen-image"
+          />
+        </div>
+        <hr className="horizontal-line" />
+        <div className="bottom-card">
+          <div className="bottom-card-heading">
+            <p className="comments-count">{noOfComments}</p>
+            <p>Comments</p>
+          </div>
+          <ul className="unordered-list">
+            {commentsList.map(eachUser => (
+              <CommentItem
+                key={eachUser.id}
+                user={eachUser}
+                bgColorsList={initialContainerBackgroundClassNames}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
